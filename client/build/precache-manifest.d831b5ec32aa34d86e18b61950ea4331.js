d7cf02ce5e665d4d03a9c9116dfb5221ec84f3f2
Curtis Simpson <crsimpson5@gmail.com>
2019-04-21 21:23:29 -0500

    production build

---
+++ client/build/precache-manifest.d831b5ec32aa34d86e18b61950ea4331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9bc45f67bc7aeb273ae5",
+    "url": "/static/js/main.2ca94667.chunk.js"
+  },
+  {
+    "revision": "92eb3052de4d9a3b9cb6",
+    "url": "/static/js/2.6fa07da0.chunk.js"
+  },
+  {
+    "revision": "9bc45f67bc7aeb273ae5",
+    "url": "/static/css/main.15e732a0.chunk.css"
+  },
+  {
+    "revision": "b5283af06ddac6938fcf8e3a325faf14",
+    "url": "/index.html"
+  }
+];
